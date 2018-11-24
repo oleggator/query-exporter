2d535ab96d12fb9d2bf0b3d88c3a223e8e6b6a20
oleggator <oleg@utkin.xyz>
2018-11-25 00:14:56 +0300

    add comments to workerpool

--- workerpool.go
+++ workerpool.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 )
 
+// WorkerPool manages goroutine pool
 type WorkerPool struct {
 	workerFunc   func(id int, tasks <-chan interface{})
 	workersCount int
@@ -11,6 +12,7 @@ type WorkerPool struct {
 	wg           sync.WaitGroup
 }
 
+// NewWorkerPool creates WorkerPool
 func NewWorkerPool(workersCount int, workerFunc func(id int, tasks <-chan interface{})) WorkerPool {
 	return WorkerPool{
 		workersCount: workersCount,
@@ -19,6 +21,7 @@ func NewWorkerPool(workersCount int, workerFunc func(id int, tasks <-chan interf
 	}
 }
 
+// Run starts goroutines and tasks handling
 func (wp *WorkerPool) Run() {
 	for i := 0; i < wp.workersCount; i++ {
 		wp.wg.Add(1)
@@ -30,18 +33,22 @@ func (wp *WorkerPool) Run() {
 	}
 }
 
+// Wait waits until all goroutines stopped
 func (wp *WorkerPool) Wait() {
 	wp.wg.Wait()
 }
 
+// Shutdown closes channel
 func (wp *WorkerPool) Shutdown() {
 	close(wp.tasks)
 }
 
+// GetInputChannel returns tasks input channel
 func (wp *WorkerPool) GetInputChannel() chan<- interface{} {
 	return wp.tasks
 }
 
+// GetWorkersCount returns workers count
 func (wp *WorkerPool) GetWorkersCount() int {
 	return wp.workersCount
 }
