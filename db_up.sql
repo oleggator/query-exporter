f9ae07e5893339faabe7695b06919238e6c6b38d
oleggator <oleg@utkin.xyz>
2018-11-24 23:26:29 +0300

    change db scheme

--- db_up.sql
+++ db_up.sql
@@ -1,10 +1,21 @@
-create table people(
-  name text,
-  lastname text,
-  birthday timestamp
+create table people (
+  id serial not null primary key,
+  name varchar(256) not null,
+  lastname varchar(256) not null,
+  birthday date not null,
+  some_flag integer not null,
+  created timestamp(0) not null default current_timestamp
 );
 
 create table cities (
-  name text,
-  country text
+  id serial not null primary key,
+  name varchar(256) not null,
+  country_id integer not null,
+  created timestamp(0) not null default current_timestamp
+);
+
+create table countries (
+  id serial not null primary key,
+  name varchar(256) not null,
+  created timestamp(0) not null default current_timestamp
 );
